02528cb5bed416eb9692315c16b79afe8c11d2bb
Rustam <rustam.zeligotov@gmail.com>
2025-08-25 19:53:39 +0200

    m

--- struct.c
+++ struct.c
@@ -3,7 +3,14 @@
 #include <string.h>
 
 TODO todos[100];
-
+void Tasks()
+    FILE *fp = fopen("tasks.txt", "r");
+if (fp == NULL) {
+  printf("Fehler bei lesen der Datei!\n");
+  return;
+  }
+  char line[300];//Max pro Zeile
+  printf(
 //input title
 void title() {
     for (int i = 0; i < 100; i++) {
