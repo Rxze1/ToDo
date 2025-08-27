aa241ece6c7617c5d9bcbceeb3d6e66292ecf60f
Rxze1 <rustam.zeligotov@gmail.com>
2025-08-27 22:00:14 +0200

    Add files via upload

--- struct.h
+++ struct.h
@@ -3,9 +3,10 @@
 
 typedef struct TODO{
 char header[20];
-char description[100];
-char editor[15];
-int duration;
+char description[500];
+    char deadline[20];
+//char editor[15];      //later
+//int duration;         //later
 int status; //0 = open, 1= finisched
 }TODO;
 
