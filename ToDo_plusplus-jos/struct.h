474b97980cd125f672de1c0ec373027b70bba8cf
Rxze1 <rustam.zeligotov@gmail.com>
2025-08-27 20:02:28 +0200

    Add files via upload
    Deadline für Todos implementiert. Todos werden abhängig von der Deadline gelöscht

---
+++ ToDo_plusplus-jos/struct.h
@@ -0,0 +1,20 @@
+#ifndef STRUCT_C
+#define STRUCT_C
+
+typedef struct TODO{
+char header[20];
+char description[500];
+    char deadline[20];
+//char editor[15];      //later
+//int duration;         //later
+int status; //0 = open, 1= finisched
+}TODO;
+
+
+
+
+
+
+
+
+#endif
